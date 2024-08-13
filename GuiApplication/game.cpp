a654b9f7a2fe4bc0ecb71e257a2bc74a3adcfaca
wu dongliang <abc8333@gmail.com>
2024-08-14 01:49:16 +0800

    fix game_finish bug && record_next bug

--- GuiApplication/game.cpp
+++ GuiApplication/game.cpp
@@ -40,8 +40,13 @@ void Game::slot_finish_game()
 
 void Game::end_finish_game()
 {
+<<<<<<< HEAD
     SHOW_DEBUG_INFO
     //over
+=======
+    SHOW_DEBUG_INFO;
+    if (m_is_recording) { m_is_recording = false; }
+>>>>>>> 16bc7a1 (fix game_finish bug && record_next bug)
 }
 
 void Game::slot_shuffle_blue_deck()
@@ -67,8 +72,451 @@ void Game::slot_shuffle_red_deck()
 
 void Game::end_shuffle_red_deck()
 {
+<<<<<<< HEAD
     SHOW_DEBUG_INFO
     //change phase
     //do draw_blue
     // emit begin_draw_blue();
+=======
+    SHOW_DEBUG_INFO;
+    if(m_is_recording) { slot_record_next(); }
+}
+
+void Game::slot_phase_blue_draw()
+{
+    SHOW_DEBUG_INFO;
+    if(!m_is_recording) { m_record << QString("%1\n").arg(__func__); }
+
+    m_phase = PhaseBlueDraw;
+    qDebug() << "change phase to" << m_phase;
+    emit begin_phase_blue_draw();
+}
+
+void Game::end_phase_blue_draw()
+{
+    SHOW_DEBUG_INFO;
+    if(m_is_recording) { slot_record_next(); }
+}
+
+void Game::slot_phase_blue_standby()
+{
+    SHOW_DEBUG_INFO;
+    if(!m_is_recording) { m_record << QString("%1\n").arg(__func__); }
+
+    m_phase = PhaseBlueStandby;
+    qDebug() << "change phase to" << m_phase;
+    emit begin_phase_blue_standby();
+}
+
+void Game::end_phase_blue_standby()
+{
+    SHOW_DEBUG_INFO;
+    if(m_is_recording) { slot_record_next(); }
+}
+
+void Game::slot_phase_blue_main1()
+{
+    SHOW_DEBUG_INFO;
+    if(!m_is_recording) { m_record << QString("%1\n").arg(__func__); }
+
+    m_phase = PhaseBlueMain1;
+    qDebug() << "change phase to" << m_phase;
+    emit begin_phase_blue_main1();
+}
+
+void Game::end_phase_blue_main1()
+{
+    SHOW_DEBUG_INFO;
+    if(m_is_recording) { slot_record_next(); }
+}
+
+void Game::slot_phase_blue_battle()
+{
+    SHOW_DEBUG_INFO;
+    if(!m_is_recording) { m_record << QString("%1\n").arg(__func__); }
+
+    m_phase = PhaseBlueBattle;
+    qDebug() << "change phase to" << m_phase;
+    emit begin_phase_blue_battle();
+}
+
+void Game::end_phase_blue_battle()
+{
+    SHOW_DEBUG_INFO;
+    if(m_is_recording) { slot_record_next(); }
+}
+
+void Game::slot_phase_blue_main2()
+{
+    SHOW_DEBUG_INFO;
+    if(!m_is_recording) { m_record << QString("%1\n").arg(__func__); }
+
+    m_phase = PhaseBlueMain2;
+    qDebug() << "change phase to" << m_phase;
+    emit begin_phase_blue_main2();
+}
+
+void Game::end_phase_blue_main2()
+{
+    SHOW_DEBUG_INFO;
+    if(m_is_recording) { slot_record_next(); }
+}
+
+void Game::slot_phase_blue_end()
+{
+    SHOW_DEBUG_INFO;
+    if(!m_is_recording) { m_record << QString("%1\n").arg(__func__); }
+
+    m_phase = PhaseBlueEnd;
+    qDebug() << "change phase to" << m_phase;
+    emit begin_phase_blue_end();
+}
+
+void Game::end_phase_blue_end()
+{
+    SHOW_DEBUG_INFO;
+    if(m_is_recording) { slot_record_next(); }
+}
+
+void Game::slot_phase_red_draw()
+{
+    SHOW_DEBUG_INFO;
+    if(!m_is_recording) { m_record << QString("%1\n").arg(__func__); }
+
+    m_phase = PhaseRedDraw;
+    qDebug() << "change phase to" << m_phase;
+    emit begin_phase_red_draw();
+}
+
+void Game::end_phase_red_draw()
+{
+    SHOW_DEBUG_INFO;
+    if(m_is_recording) { slot_record_next(); }
+}
+
+void Game::slot_phase_red_standby()
+{
+    SHOW_DEBUG_INFO;
+    if(!m_is_recording) { m_record << QString("%1\n").arg(__func__); }
+
+    m_phase = PhaseRedStandby;
+    qDebug() << "change phase to" << m_phase;
+    emit begin_phase_red_standby();
+}
+
+void Game::end_phase_red_standby()
+{
+    SHOW_DEBUG_INFO;
+    if(m_is_recording) { slot_record_next(); }
+}
+
+void Game::slot_phase_red_main1()
+{
+    SHOW_DEBUG_INFO;
+    if(!m_is_recording) { m_record << QString("%1\n").arg(__func__); }
+
+    m_phase = PhaseRedMain1;
+    qDebug() << "change phase to" << m_phase;
+    emit begin_phase_red_main1();
+}
+
+void Game::end_phase_red_main1()
+{
+    SHOW_DEBUG_INFO;
+    if(m_is_recording) { slot_record_next(); }
+}
+
+void Game::slot_phase_red_battle()
+{
+    SHOW_DEBUG_INFO;
+    if(!m_is_recording) { m_record << QString("%1\n").arg(__func__); }
+
+    m_phase = PhaseRedBattle;
+    qDebug() << "change phase to" << m_phase;
+    emit begin_phase_red_battle();
+}
+
+void Game::end_phase_red_battle()
+{
+    SHOW_DEBUG_INFO;
+    if(m_is_recording) { slot_record_next(); }
+}
+
+void Game::slot_phase_red_main2()
+{
+    SHOW_DEBUG_INFO;
+    if(!m_is_recording) { m_record << QString("%1\n").arg(__func__); }
+
+    m_phase = PhaseRedMain2;
+    qDebug() << "change phase to" << m_phase;
+    emit begin_phase_red_main2();
+}
+
+void Game::end_phase_red_main2()
+{
+    SHOW_DEBUG_INFO;
+    if(m_is_recording) { slot_record_next(); }
+}
+
+void Game::slot_phase_red_end()
+{
+    SHOW_DEBUG_INFO;
+    if(!m_is_recording) { m_record << QString("%1\n").arg(__func__); }
+
+    m_phase = PhaseRedEnd;
+    qDebug() << "change phase to" << m_phase;
+    emit begin_phase_red_end();
+}
+
+void Game::end_phase_red_end()
+{
+    SHOW_DEBUG_INFO;
+    if(m_is_recording) { slot_record_next(); }
+}
+
+int Game::check_blue_front()
+{
+    if(blue_front_1 == nullptr)
+    {
+        return 1;
+    }
+    else if(blue_front_2 == nullptr)
+    {
+        return 2;
+    }
+    else if(blue_front_3 == nullptr)
+    {
+        return 3;
+    }
+    else if(blue_front_4 == nullptr)
+    {
+        return 4;
+    }
+    else if(blue_front_5 == nullptr)
+    {
+        return 5;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int Game::check_blue_back()
+{
+    if(blue_back_1 == nullptr)
+    {
+        return 1;
+    }
+    else if(blue_back_2 == nullptr)
+    {
+        return 2;
+    }
+    else if(blue_back_3 == nullptr)
+    {
+        return 3;
+    }
+    else if(blue_back_4 == nullptr)
+    {
+        return 4;
+    }
+    else if(blue_back_5 == nullptr)
+    {
+        return 5;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int Game::check_red_front()
+{
+    if(red_front_1 == nullptr)
+    {
+        return 1;
+    }
+    else if(red_front_2 == nullptr)
+    {
+        return 2;
+    }
+    else if(red_front_3 == nullptr)
+    {
+        return 3;
+    }
+    else if(red_front_4 == nullptr)
+    {
+        return 4;
+    }
+    else if(red_front_5 == nullptr)
+    {
+        return 5;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int Game::check_red_back()
+{
+    if(red_back_1 == nullptr)
+    {
+        return 1;
+    }
+    else if(red_back_2 == nullptr)
+    {
+        return 2;
+    }
+    else if(red_back_3 == nullptr)
+    {
+        return 3;
+    }
+    else if(red_back_4 == nullptr)
+    {
+        return 4;
+    }
+    else if(red_back_5 == nullptr)
+    {
+        return 5;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+void Game::slot_blue_draw_cards(int size)
+{
+    SHOW_DEBUG_INFO;
+    m_need_blue_draw = size;
+    qDebug() << "need blue draw cards" << m_need_blue_draw;
+    emit begin_blue_draw_cards(size);
+}
+
+void Game::end_blue_draw_cards(int size)
+{
+    Q_UNUSED(size)
+    SHOW_DEBUG_INFO;
+}
+
+void Game::slot_red_draw_cards(int size)
+{
+    SHOW_DEBUG_INFO;
+    m_need_red_draw = size;
+    qDebug() << "need red draw cards" << m_need_red_draw;
+    emit begin_red_draw_cards(size);
+}
+
+void Game::end_red_draw_cards(int size)
+{
+    Q_UNUSED(size)
+    SHOW_DEBUG_INFO;
+}
+
+void Game::slot_record_save()
+{
+    SHOW_DEBUG_INFO;
+    emit begin_record_save(m_record);
+}
+
+void Game::slot_record_load(QStringList list)
+{
+    SHOW_DEBUG_INFO;
+    m_record = list;
+    m_is_recording = true;
+
+    slot_record_next();
+}
+
+void Game::slot_record_next()
+{
+    QString record_line = m_record.takeFirst();
+    QStringList record_line_list = record_line.split(',');
+    QString slot = record_line_list.first();
+    qDebug() << slot;
+    if(slot == "slot_move_card")
+    {
+        int card_sn = QString(record_line_list.at(1)).toInt();
+        QString from_area = record_line_list.at(2);
+        int from_index = QString(record_line_list.at(3)).toInt();
+        QString to_area = record_line_list.at(4);
+        int to_index = QString(record_line_list.at(5)).toInt();
+        QMetaObject::invokeMethod(this, "slot_move_card", Q_ARG(int, card_sn), Q_ARG(QString, from_area), Q_ARG(int, from_index), Q_ARG(QString, to_area), Q_ARG(int, to_index));
+    }
+    else if(slot == "slot_effect_card")
+    {
+        int card_sn = QString(record_line_list.at(1)).toInt();
+        QString from_area = record_line_list.at(2);
+        int from_index = QString(record_line_list.at(3)).toInt();
+        QMetaObject::invokeMethod(this, "slot_effect_card", Q_ARG(int, card_sn), Q_ARG(QString, from_area), Q_ARG(int, from_index));
+    }
+    else if(slot == "slot_try_card")
+    {
+        int card_sn = QString(record_line_list.at(1)).toInt();
+        QString from_area = record_line_list.at(2);
+        int from_index = QString(record_line_list.at(3)).toInt();
+        QMetaObject::invokeMethod(this, "slot_try_card", Q_ARG(int, card_sn), Q_ARG(QString, from_area), Q_ARG(int, from_index));
+    }
+    else
+    {
+        QMetaObject::invokeMethod(this, slot.toUtf8().data());
+    }
+}
+
+void Game::slot_move_card(int card_sn, QString from_area, int from_index, QString to_area, int to_index)
+{
+    SHOW_DEBUG_INFO;
+    if(!m_is_recording) { m_record << QString("%1,%2,%3,%4,%5,%6\n").arg(__func__).arg(card_sn).arg(from_area).arg(from_index).arg(to_area).arg(to_index); }
+
+    qDebug() << card_sn << from_area << from_index << to_area << to_index;
+    emit begin_move_card(card_sn, from_area, from_index, to_area, to_index);
+}
+
+void Game::end_move_card(int card_sn, QString from_area, int from_index, QString to_area, int to_index)
+{
+    Q_UNUSED(card_sn)
+    Q_UNUSED(from_area)
+    Q_UNUSED(from_index)
+    Q_UNUSED(to_area)
+    Q_UNUSED(to_index)
+    SHOW_DEBUG_INFO;
+    if(m_is_recording) { slot_record_next(); }
+}
+
+void Game::slot_effect_card(int card_sn, QString from_area, int from_index)
+{
+    SHOW_DEBUG_INFO;
+    if(!m_is_recording) { m_record << QString("%1,%2,%3,%4\n").arg(__func__).arg(card_sn).arg(from_area).arg(from_index); }
+
+    qDebug() << card_sn << from_area << from_index;
+    emit begin_effect_card(card_sn, from_area, from_index);
+}
+
+void Game::end_effect_card(int card_sn, QString from_area, int from_index)
+{
+    Q_UNUSED(card_sn)
+    Q_UNUSED(from_area)
+    Q_UNUSED(from_index)
+    SHOW_DEBUG_INFO;
+    if(m_is_recording) { slot_record_next(); }
+}
+
+void Game::slot_try_card(int card_sn, QString from_area, int from_index)
+{
+    SHOW_DEBUG_INFO;
+    if(!m_is_recording) { m_record << QString("%1,%2,%3,%4\n").arg(__func__).arg(card_sn).arg(from_area).arg(from_index); }
+
+    qDebug() << card_sn << from_area << from_index;
+    emit begin_try_card(card_sn, from_area, from_index);
+}
+
+void Game::end_try_card(int card_sn, QString from_area, int from_index)
+{
+    Q_UNUSED(card_sn)
+    Q_UNUSED(from_area)
+    Q_UNUSED(from_index)
+    SHOW_DEBUG_INFO;
+    if(m_is_recording) { slot_record_next(); }
+>>>>>>> 16bc7a1 (fix game_finish bug && record_next bug)
 }
--- GuiApplication/mainwindow.cpp
+++ GuiApplication/mainwindow.cpp
@@ -90,3 +90,314 @@ void MainWindow::begin_shuffle_red_deck()
     //animate shuffle_red_deck
     emit end_shuffle_red_deck();
 }
+<<<<<<< HEAD
+=======
+
+void MainWindow::on_phase_blue_draw_triggered()
+{
+    SHOW_DEBUG_INFO;
+    emit signal_phase_blue_draw();
+}
+
+void MainWindow::begin_phase_blue_draw()
+{
+    SHOW_DEBUG_INFO;
+    //animate phase_blue_draw
+    emit end_phase_blue_draw();
+}
+
+void MainWindow::on_phase_blue_standby_triggered()
+{
+    SHOW_DEBUG_INFO;
+    emit signal_phase_blue_standby();
+}
+
+void MainWindow::begin_phase_blue_standby()
+{
+    SHOW_DEBUG_INFO;
+    //animate phase_blue_standby
+    emit end_phase_blue_standby();
+}
+
+void MainWindow::on_phase_blue_main1_triggered()
+{
+    SHOW_DEBUG_INFO;
+    emit signal_phase_blue_main1();
+}
+
+void MainWindow::begin_phase_blue_main1()
+{
+    SHOW_DEBUG_INFO;
+    //animate phase_blue_main1
+    emit end_phase_blue_main1();
+}
+
+void MainWindow::on_phase_blue_battle_triggered()
+{
+    SHOW_DEBUG_INFO;
+    emit signal_phase_blue_battle();
+}
+
+void MainWindow::begin_phase_blue_battle()
+{
+    SHOW_DEBUG_INFO;
+    //animate phase_blue_battle
+    emit end_phase_blue_battle();
+}
+
+void MainWindow::on_phase_blue_main2_triggered()
+{
+    SHOW_DEBUG_INFO;
+    emit signal_phase_blue_main2();
+}
+
+void MainWindow::begin_phase_blue_main2()
+{
+    SHOW_DEBUG_INFO;
+    //animate phase_blue_main2
+    emit end_phase_blue_main2();
+}
+
+void MainWindow::on_phase_blue_end_triggered()
+{
+    SHOW_DEBUG_INFO;
+    emit signal_phase_blue_end();
+}
+
+void MainWindow::begin_phase_blue_end()
+{
+    SHOW_DEBUG_INFO;
+    //animate phase_blue_end
+    emit end_phase_blue_end();
+}
+
+void MainWindow::on_phase_red_draw_triggered()
+{
+    SHOW_DEBUG_INFO;
+    emit signal_phase_red_draw();
+}
+
+void MainWindow::begin_phase_red_draw()
+{
+    SHOW_DEBUG_INFO;
+    //animate phase_red_draw
+    emit end_phase_red_draw();
+}
+
+void MainWindow::on_phase_red_standby_triggered()
+{
+    SHOW_DEBUG_INFO;
+    emit signal_phase_red_standby();
+}
+
+void MainWindow::begin_phase_red_standby()
+{
+    SHOW_DEBUG_INFO;
+    //animate phase_red_standby
+    emit end_phase_red_standby();
+}
+
+void MainWindow::on_phase_red_main1_triggered()
+{
+    SHOW_DEBUG_INFO;
+    emit signal_phase_red_main1();
+}
+
+void MainWindow::begin_phase_red_main1()
+{
+    SHOW_DEBUG_INFO;
+    //animate phase_red_main1
+    emit end_phase_red_main1();
+}
+
+void MainWindow::on_phase_red_battle_triggered()
+{
+    SHOW_DEBUG_INFO;
+    emit signal_phase_red_battle();
+}
+
+void MainWindow::begin_phase_red_battle()
+{
+    SHOW_DEBUG_INFO;
+    //animate phase_red_battle
+    emit end_phase_red_battle();
+}
+
+void MainWindow::on_phase_red_main2_triggered()
+{
+    SHOW_DEBUG_INFO;
+    emit signal_phase_red_main2();
+}
+
+void MainWindow::begin_phase_red_main2()
+{
+    SHOW_DEBUG_INFO;
+    //animate phase_red_main2
+    emit end_phase_red_main2();
+}
+
+void MainWindow::on_phase_red_end_triggered()
+{
+    SHOW_DEBUG_INFO;
+    emit signal_phase_red_end();
+}
+
+void MainWindow::begin_phase_red_end()
+{
+    SHOW_DEBUG_INFO;
+    //animate phase_red_end
+    emit end_phase_red_end();
+}
+
+void MainWindow::on_blue_draw_one_triggered()
+{
+    SHOW_DEBUG_INFO;
+    emit signal_blue_draw_cards(1);
+}
+
+void MainWindow::on_blue_draw_two_triggered()
+{
+    SHOW_DEBUG_INFO;
+    emit signal_blue_draw_cards(2);
+}
+
+void MainWindow::on_blue_draw_three_triggered()
+{
+    SHOW_DEBUG_INFO;
+    emit signal_blue_draw_cards(3);
+}
+
+void MainWindow::on_blue_draw_four_triggered()
+{
+    SHOW_DEBUG_INFO;
+    emit signal_blue_draw_cards(4);
+}
+
+void MainWindow::on_blue_draw_five_triggered()
+{
+    SHOW_DEBUG_INFO;
+    emit signal_blue_draw_cards(5);
+}
+
+void MainWindow::begin_blue_draw_cards(int size)
+{
+    SHOW_DEBUG_INFO;
+    //animate blue_draw_cards
+    emit end_blue_draw_cards(size);
+}
+
+void MainWindow::on_red_draw_one_triggered()
+{
+    SHOW_DEBUG_INFO;
+    emit signal_red_draw_cards(1);
+}
+
+void MainWindow::on_red_draw_two_triggered()
+{
+    SHOW_DEBUG_INFO;
+    emit signal_red_draw_cards(2);
+}
+
+void MainWindow::on_red_draw_three_triggered()
+{
+    SHOW_DEBUG_INFO;
+    emit signal_red_draw_cards(3);
+}
+
+void MainWindow::on_red_draw_four_triggered()
+{
+    SHOW_DEBUG_INFO;
+    emit signal_red_draw_cards(4);
+}
+
+void MainWindow::on_red_draw_five_triggered()
+{
+    SHOW_DEBUG_INFO;
+    emit signal_red_draw_cards(5);
+}
+
+void MainWindow::begin_red_draw_cards(int size)
+{
+    SHOW_DEBUG_INFO;
+    //animate red_draw_cards
+    emit end_red_draw_cards(size);
+}
+
+void MainWindow::on_record_load_triggered()
+{
+    SHOW_DEBUG_INFO;
+    QString fileName = QFileDialog::getOpenFileName(this, "record load", "C:/Users/Administrator/Desktop/Git/dota_card/GuiApplication", "*.csv");
+    QFile file(fileName);
+    file.open(QIODevice::ReadOnly|QIODevice::Text);
+    QStringList list;
+    while(!file.atEnd())
+    {
+        QString str = file.readLine().simplified();
+        list << str;
+    }
+    file.close();
+
+    qDebug() << "record list size:" << list.size();
+    emit signal_record_load(list);
+}
+
+void MainWindow::on_record_next_triggered()
+{
+    SHOW_DEBUG_INFO;
+    emit signal_record_next();
+}
+
+void MainWindow::on_record_save_triggered()
+{
+    SHOW_DEBUG_INFO;
+    emit signal_record_save();
+}
+
+void MainWindow::begin_record_save(QStringList list)
+{
+    SHOW_DEBUG_INFO;
+    QFile file(QString("C:/Users/Administrator/Desktop/Git/dota_card/GuiApplication/%1.csv").arg(QTime::currentTime().toString("record_hhmmss")));
+    file.open(QIODevice::WriteOnly|QIODevice::Text);
+    for(QString str : list)
+    {
+        file.write(str.toUtf8());
+    }
+    file.close();
+}
+
+void MainWindow::on_move_card_clicked()
+{
+    SHOW_DEBUG_INFO;
+    emit signal_move_card(ui->card_sn_move->text().toInt(), ui->from_area_move->currentText(), ui->from_index_move->text().toInt(), ui->to_area_move->currentText(), ui->to_index_move->text().toInt());
+}
+
+void MainWindow::begin_move_card(int card_sn, QString from_area, int from_index, QString to_area, int to_index)
+{
+    SHOW_DEBUG_INFO;
+    emit end_move_card(card_sn, from_area, from_index, to_area, to_index);
+}
+
+void MainWindow::on_effect_card_clicked()
+{
+    SHOW_DEBUG_INFO;
+    emit signal_effect_card(ui->card_sn_effect->text().toInt(), ui->from_area_effect->currentText(), ui->from_index_effect->text().toInt());
+}
+
+void MainWindow::begin_effect_card(int card_sn, QString from_area, int from_index)
+{
+    SHOW_DEBUG_INFO;
+    emit end_effect_card(card_sn, from_area, from_index);
+}
+
+void MainWindow::on_try_card_clicked()
+{
+    SHOW_DEBUG_INFO;
+    emit signal_try_card(ui->card_sn_try->text().toInt(), ui->from_area_try->currentText(), ui->from_index_try->text().toInt());
+}
+
+void MainWindow::begin_try_card(int card_sn, QString from_area, int from_index)
+{
+    SHOW_DEBUG_INFO;
+    emit end_try_card(card_sn, from_area, from_index);
+}
+>>>>>>> 16bc7a1 (fix game_finish bug && record_next bug)
